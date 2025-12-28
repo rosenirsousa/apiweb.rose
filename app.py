916ed3b68b922601c4688b137c4510e5c1cfe1ba
Rosenir <rosenirsousa@icloud.com>
2025-12-28 10:37:19 -0400

    Adicionando arquivos finais do projeto

---
+++ app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.header('Jogando uma moeda')
+
+st.write('Ainda não é um aplicativo funcional. Em construção.')
+
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('vehicles.csv') # lendo os dados
+hist_button = st.button('Criar histograma') # criar um botão
+
+if hist_button: # se o botão for clicado
+    # escrever uma mensagem
+    st.write('Criando um histograma para o conjunto de dados de anúncios de vendas de carros')
+    
+    # criar um histograma
+    fig = px.histogram(car_data, x="odometer")
+
+    # exibir um gráfico Plotly interativo
+    st.plotly_chart(fig, use_container_width=True)
+
+
+import streamlit as st
+
+# criar uma caixa de seleção
+build_histogram = st.checkbox('Criar um histograma')
+
+if build_histogram: # se a caixa de seleção for selecionada
+  st.write('Criando um histograma para a coluna odometer')
